c21711fc2a5969bd56ac9f2b8dff8000342dc9c2
Francesco Pozzobon <francesco.pozzobon.1@studenti.unipd.it>
2024-05-15 16:20:07 +0200

    close #25; Implementati test conversione primi 100 numeri e posti @Ignore

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -56,4 +56,71 @@ public class RomanPrinterTest {
         assertEquals(var49, RomanPrinter.print(49));
         assertEquals(var50, RomanPrinter.print(50));
     }
+
+    @Ignore
+    public void checkPrintPrimiCento() {
+        String var51 =" _       ___  \n" +
+                "| |     |_ _| \n" +
+                "| |      | |  \n" +
+                "| |___   | |  \n" +
+                "|_____| |___| \n";
+        String var57 =" _      __     __  ___   ___  \n" +
+                "| |     \\ \\   / / |_ _| |_ _| \n" +
+                "| |      \\ \\ / /   | |   | |  \n" +
+                "| |___    \\ V /    | |   | |  \n" +
+                "|_____|    \\_/    |___| |___| \n";
+        String var62 =" _      __  __  ___   ___  \n" +
+                "| |     \\ \\/ / |_ _| |_ _| \n" +
+                "| |      \\  /   | |   | |  \n" +
+                "| |___   /  \\   | |   | |  \n" +
+                "|_____| /_/\\_\\ |___| |___| \n";
+        String var68 =" _      __  __ __     __  ___   ___   ___  \n" +
+                "| |     \\ \\/ / \\ \\   / / |_ _| |_ _| |_ _| \n" +
+                "| |      \\  /   \\ \\ / /   | |   | |   | |  \n" +
+                "| |___   /  \\    \\ V /    | |   | |   | |  \n" +
+                "|_____| /_/\\_\\    \\_/    |___| |___| |___| \n";
+        String var70 =" _      __  __ __  __ \n" +
+                "| |     \\ \\/ / \\ \\/ / \n" +
+                "| |      \\  /   \\  /  \n" +
+                "| |___   /  \\   /  \\  \n" +
+                "|_____| /_/\\_\\ /_/\\_\\ \n";
+        String var73 =" _      __  __ __  __  ___   ___   ___  \n" +
+                "| |     \\ \\/ / \\ \\/ / |_ _| |_ _| |_ _| \n" +
+                "| |      \\  /   \\  /   | |   | |   | |  \n" +
+                "| |___   /  \\   /  \\   | |   | |   | |  \n" +
+                "|_____| /_/\\_\\ /_/\\_\\ |___| |___| |___| \n";
+        String var84 =" _      __  __ __  __ __  __  ___  __     __ \n" +
+                "| |     \\ \\/ / \\ \\/ / \\ \\/ / |_ _| \\ \\   / / \n" +
+                "| |      \\  /   \\  /   \\  /   | |   \\ \\ / /  \n" +
+                "| |___   /  \\   /  \\   /  \\   | |    \\ V /   \n" +
+                "|_____| /_/\\_\\ /_/\\_\\ /_/\\_\\ |___|    \\_/    \n";
+        String var86 =" _      __  __ __  __ __  __ __     __  ___  \n" +
+                "| |     \\ \\/ / \\ \\/ / \\ \\/ / \\ \\   / / |_ _| \n" +
+                "| |      \\  /   \\  /   \\  /   \\ \\ / /   | |  \n" +
+                "| |___   /  \\   /  \\   /  \\    \\ V /    | |  \n" +
+                "|_____| /_/\\_\\ /_/\\_\\ /_/\\_\\    \\_/    |___| \n";
+        String var95 ="__  __   ____  __     __ \n" +
+                "\\ \\/ /  / ___| \\ \\   / / \n" +
+                " \\  /  | |      \\ \\ / /  \n" +
+                " /  \\  | |___    \\ V /   \n" +
+                "/_/\\_\\  \\____|    \\_/    \n";
+        String var99 ="__  __   ____   ___  __  __ \n" +
+                "\\ \\/ /  / ___| |_ _| \\ \\/ / \n" +
+                " \\  /  | |      | |   \\  /  \n" +
+                " /  \\  | |___   | |   /  \\  \n" +
+                "/_/\\_\\  \\____| |___| /_/\\_\\ \n";
+
+        assertEquals(var51, RomanPrinter.print(51));
+        assertEquals(var57, RomanPrinter.print(57));
+        assertEquals(var62, RomanPrinter.print(62));
+        assertEquals(var68, RomanPrinter.print(68));
+        assertEquals(var70, RomanPrinter.print(70));
+        assertEquals(var73, RomanPrinter.print(73));
+        assertEquals(var84, RomanPrinter.print(84));
+        assertEquals(var86, RomanPrinter.print(86));
+        assertEquals(var95, RomanPrinter.print(95));
+        assertEquals(var99, RomanPrinter.print(99));
+
+    }
+
 }
