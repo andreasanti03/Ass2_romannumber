5c97cb352e9041bd2f38cd1cc75260f622c4031d
Francesco Pozzobon <francesco.pozzobon.1@studenti.unipd.it>
2024-05-15 16:33:09 +0200

    close #30; implementato test controllo stampa zero

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -5,10 +5,22 @@
 package it.unipd.mtss;
 
 import org.junit.Test;
+import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
 
 public class RomanPrinterTest {
+
+    @Before
+    public void checkPrintZero(){
+        String var0 = "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n";
+        assertEquals(var0, RomanPrinter.print(0));
+    }
+
     @Test
     public void checkPrintPrimiTre(){
         String var1 = " ___  \n" +
